6b573f49b5bcb408c811828a2413bb171d4e0f3a
Rogulin Sergey <sv.rogulin@gmail.com>
2022-10-19 18:18:56 +0300

    Updated composer config, added postgres & redis images

--- backend/php/scripts/start.sh
+++ backend/php/scripts/start.sh
@@ -5,11 +5,6 @@ if [ -f "/var/www/composer.json" ]; then
     $(which composer) install --working-dir=/var/www
 fi
 
-if [ -f "/var/www/package.json" ]; then
-    echo "INSTALLING NODEJS DEPENDENCIES..."
-    $(which npm) install --prefix=/var/www
-fi
-
 echo "INSTALLING PHP PACKAGES..."
 $(which composer) global require \
     phpstan/phpstan \
@@ -20,20 +15,10 @@ $(which composer) global require \
 echo "DOWNLOADING PHP PACKAGES..."
 wget -O /usr/local/bin/phploc https://phar.phpunit.de/phploc.phar \
     && chmod +x /usr/local/bin/phploc
-wget -O /usr/local/bin/phpchck https://github.com/fabpot/local-php-security-checker/releases/download/v1.0.0/local-php-security-checker_1.0.0_linux_amd64 \
+wget -O /usr/local/bin/phpchck https://github.com/fabpot/local-php-security-checker/releases/download/v2.0.5/local-php-security-checker_2.0.5_linux_amd64 \
     && chmod +x /usr/local/bin/phpchck
 
-echo "INSTALLING NODEJS PACKAGES..."
-$(which npm) install --global \
-    yarn \
-    svgo \
-    nodemon \
-    typescript \
-    @types/node
-
 echo "CHANGING PERMISSIONS..."
-touch /var/log/node/tsc.log
-chown www-data:www-data /var/log/node/tsc.log
 chown www-data:www-data /var/log/php/*
 chown -R www-data:www-data /var/www
 
